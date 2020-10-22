9187bcb1a52f822bfb9647e3cd9b075b5a32f12a
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-10-22 13:29:02 +0200

    Extract outputMessages and inputMessages into separate extension functions to make code more straightforward

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -4,38 +4,46 @@ import io.ktor.client.*
 import io.ktor.client.features.websocket.*
 import io.ktor.http.*
 import io.ktor.http.cio.websocket.*
-import kotlinx.coroutines.*
+import io.ktor.util.*
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
+suspend fun DefaultClientWebSocketSession.outputMessages() {
+    try {
+        for (message in incoming) {
+            message as? Frame.Text ?: continue
+            println(message.readText())
+        }
+    } catch (e: Exception) {
+        println("Error while receiving: " + e.localizedMessage)
+    }
+}
+
+suspend fun DefaultClientWebSocketSession.inputMessages() {
+    while (true) {
+        val message = readLine() ?: ""
+        if (message.equals("exit", true)) return
+        try {
+            send(Frame.Text(message))
+        } catch (e: Exception) {
+            println("Error while sending: " + e.localizedMessage)
+            return
+        }
+    }
+}
+
+@KtorExperimentalAPI
 fun main() {
     val client = HttpClient {
         install(WebSockets)
     }
     runBlocking {
         client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
-            val messageOutputRoutine = launch {
-                try {
-                    for (message in incoming) {
-                        message as? Frame.Text ?: continue
-                        println(message.readText())
-                    }
-                } catch (e: Exception) {
-                    println("Error while receiving: " + e.localizedMessage)
-                }
-            }
+            val messageOutputRoutine = launch { outputMessages() }
+            val userInputRoutine = launch { inputMessages() }
 
-            val userInputRoutine = launch {
-                while (true) {
-                    val message = readLine() ?: ""
-                    if (message.equals("exit", true)) return@launch
-                    try {
-                        outgoing.send(Frame.Text(message))
-                    } catch (e: Exception) {
-                        println("Error while sending: " + e.localizedMessage)
-                        return@launch
-                    }
-                }
-            }
-            userInputRoutine.join()
+            userInputRoutine.join() // Wait for completion; either "exit" or error
             messageOutputRoutine.cancelAndJoin()
         }
     }
